30b3a12c7ac5ec17f5b748ae12522599c99ba81c
FynnKummer <fynn.kummer@student.jade-hs.de>
2021-05-26 18:36:44 +0200

    Create LinkedList.cs

---
+++ 3. Sprint/Schraubengott/LinkedList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schraubengott
+{
+    class LinkedList
+    {
+        public int count = 0; //Countetr, um immer zu wissen, wie viele Elemente in der liste sind
+        LinkedListElement head;
+
+        public void AddNodToFront(int kundennr, String pass)
+        {
+            LinkedListElement node = new LinkedListElement(kundennr, pass);
+            node.next = head;
+            head = node;
+            count++;
+        }
+        public void PrintList()
+        {
+
+            LinkedListElement runner = head;
+            while (runner != null)
+            {
+                //irgendeinlable.content=runner.kundennr;
+                runner = runner.next;
+            }
+        }
+
+
+
+    }
+    class LinkedListElement
+    {
+        public int kundennr;
+        private String password;
+        public LinkedListElement next;
+
+        public LinkedListElement(int kundennr, String password)
+        {
+            this.kundennr = kundennr;
+            this.password = password;
+            next = null;
+        }
+
+
+    }
+}
