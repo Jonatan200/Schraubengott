d7eb12791302e249913547e96acc06033e03336d
Malteklm <malte.klarmann@gmail.com>
2021-05-30 17:19:43 +0200

    af

--- 3. Sprint/Schraubengott/Catia/CatiaConnection.cs
+++ 3. Sprint/Schraubengott/Catia/CatiaConnection.cs
@@ -517,10 +517,6 @@ namespace Schraubengott
             Factory2D factory2D1 = sketch1.OpenEdition();
             sketch1.CloseEdition();
 
-
-
-
-
             //Versuch Textfelder 
 
             DrawingViews drawingViews3 = drawingSheet1.Views;
@@ -541,8 +537,6 @@ namespace Schraubengott
 
             DrawingTexts texts1 = drawingView3.Texts;
             DrawingText text1 = texts1.Add("Test", 100, 100);
-
         }
-
     }
 }
